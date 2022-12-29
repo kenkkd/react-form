a191ea150362f80b9336f663b59683444d0b2452
掛田健太 <85382793+knt-k@users.noreply.github.com>
2022-12-29 17:09:36 +0900

    Add type to createContact params

--- src/firestore/createContact.ts
+++ src/firestore/createContact.ts
@@ -2,7 +2,7 @@ import { addDoc, collection } from 'firebase/firestore';
 import { db } from '~/firebase';
 import { Collection } from '~/constants/firestore';
 
-export const createContact = async (params: any) => {
+export const createContact = async (params: CreateContactParams) => {
   const { firstName, lastName, companyName } = params;
   await addDoc(collection(db, Collection.contacts), {
     firstName,
